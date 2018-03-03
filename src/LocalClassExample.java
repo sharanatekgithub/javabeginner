1f9a40d7b66098d3d19c0293646eb28e8f9f8b6e
FPLNT\SXK0W7W <sharana.kampli@nexteraenergy.com>
2018-03-03 09:15:19 -0500

    checking in

---
+++ src/LocalClassExample.java
@@ -0,0 +1,59 @@
+public class LocalClassExample {
+    static String regularExpression = "[^0-9]";
+
+    public static void validatePhoneNumber(
+            String phoneNumber1, String phoneNumber2) {
+
+        //final int numberLength = 10 ;
+        int numberLength = 10;
+
+        //Local class
+
+        class PhoneNumber {
+            String formattedPhoneNumber = null;
+
+            PhoneNumber(String phoneNumber) {
+                String currentNumber = phoneNumber.replaceAll(
+                        regularExpression, "");
+
+                if (currentNumber.length() == numberLength)
+                    formattedPhoneNumber = currentNumber;
+                else
+                    formattedPhoneNumber = null;
+
+            }  //end of constructor of Local class
+
+            public String getNumber() {
+                return formattedPhoneNumber;
+            }
+
+            public void printOriginalNumbers() {
+                System.out.println("Original numbers are " + phoneNumber1 +
+                        " and " + phoneNumber2);
+            }
+
+
+        } //end of definition of Local class
+
+        PhoneNumber myNumber1 = new PhoneNumber(phoneNumber1);
+        PhoneNumber myNumber2 = new PhoneNumber(phoneNumber2);
+
+        myNumber1.printOriginalNumbers();
+
+        if (myNumber1.getNumber() == null)
+            System.out.println("First number is invalid");
+        else
+            System.out.println("First number is " + myNumber1.getNumber());
+        if (myNumber2.getNumber() == null)
+            System.out.println("Second number is invalid");
+        else
+            System.out.println("Second number is " + myNumber2.getNumber());
+    }//end of local Method
+
+
+    public static void main(String... args) {
+        validatePhoneNumber("123-456-7890"
+                            , "456-7890");
+    }
+
+}
