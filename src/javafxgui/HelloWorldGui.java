74f98f28641dac0f447e3b7cb74d7353cfadf897
FPLNT\SXK0W7W <sharana.kampli@nexteraenergy.com>
2018-03-03 11:34:54 -0500

    checking in

---
+++ src/javafxgui/HelloWorldGui.java
@@ -0,0 +1,38 @@
+package javafxgui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class HelloWorldGui extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 600, 550));
+        primaryStage.show();
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+
+    }
+}
+
