68afa7220b158a4944ea993adc05044eced40b9c
FPLNT\SXK0W7W <sharana.kampli@nexteraenergy.com>
2018-04-07 14:18:55 -0400

    checking in

---
+++ src/ArrayAssignmentTest.java
@@ -0,0 +1,8 @@
+public class ArrayAssignmentTest {
+    public static void main(String[] args) {
+        int a[] = { 1, 2,3 };
+        float f[]={ 1.0f, 2f,3f };
+        double dbl[]=f;
+
+    }
+}
